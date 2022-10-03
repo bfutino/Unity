be792ec6833f869d23223cfefc7530065589763f
Bruno Futino <37307696+bfutino@users.noreply.github.com>
2022-10-02 20:16:55 -0700

    Add files via upload

---
+++ wasd_movement_2d/Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode jumpKey;
+    public float speed = 10f;
+
+    // regular movement with the added ability to "dash"(skip distance) in any direction when pressing the space bar and direction wanted 
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") && Input.GetKey("space"))
+        {
+            pos.y += (float) 0.2;
+        }else if(Input.GetKey("w")){
+             pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") && Input.GetKey("space"))
+        {
+            pos.y -= (float) 0.2;
+        }else if(Input.GetKey("s")){
+             pos.y -= speed * Time.deltaTime;
+        }
+
+        
+        if (Input.GetKey("d") && Input.GetKey("space"))
+        {
+            pos.x += (float) 0.2;
+        }else if(Input.GetKey("d")){
+            pos.x += speed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey("a") && Input.GetKey("space"))
+        {
+            pos.x -= (float) 0.2;
+        }else if(Input.GetKey("a")){
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+        transform.position = pos;
+    }
+}
